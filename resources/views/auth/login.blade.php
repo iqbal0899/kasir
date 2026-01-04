5f7cd6918c5361b662d95b3064d8bebd859d0294
iqbal0899 <79738794+iqbal0899@users.noreply.github.com>
2026-01-04 14:17:38 +0700

    login.blade.php

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -15,7 +15,6 @@
                         </div>
                       </div>
                     <form method="POST" action="{{ route('login') }}">
-                        @csrf
 
                         <div class="row mb-3">
                             <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
